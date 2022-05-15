013b3af02fde92e8228280947218afde1e21f930
soul <soulreviewsmail@gmail.com>
2022-05-15 14:23:54 -0400

    Custom header types, Refiling tasks, Capture templates

--- init.el
+++ init.el
@@ -291,11 +291,130 @@
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
 
 (use-package org
-	     :hook (org-mode . efs/org-mode-setup)
+  :hook (org-mode . efs/org-mode-setup)
 	     :config (setq org-ellipsis " 🎈"
 			   ;; hides formatting markers
 			   org-hide-emphasis-markers t)
-	     (efs/org-font-setup)) 
+	     (efs/org-font-setup))
+
+;; Log the time when a task was complete 
+(setq org-agenda-start-with-log-mode t)
+(setq org-log-done 'time)
+;; Fold log entries into drawer
+(setq org-log-into-drawer t)
+
+(setq org-agenda-files '("~/.emacs.provingGrounds/orgfiles/tasks.org"
+			 "~/.emacs.provingGrounds/orgfiles/birthdays.org"
+			 "~/.emacs.provingGrounds/orgfiles/habits.org"
+			 )) 
+
+(require 'org-habit)
+(add-to-list 'org-modules 'org-habit)
+(setq org-habit-graph-column 60)
+
+(setq org-todo-keywords
+      '((sequence
+	 ;; Active states
+	 "TODO(t)" "NEXT(n)"
+	 "|"
+	 ;; Done state
+	 "DONE(d!)")
+	(sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!)" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+
+(setq org-refile-targets
+      '(("archive.org" :maxlevel . 1)
+	("tasks.org" :maxlevel . 1)))
+
+(advice-add 'org-refile :after `org-save-all-org-buffers) 
+
+(setq org-tag-alist
+      '((:startgroup)
+	;; Mutually exclusive tags go here
+	(:endgroup)
+       ("@errand" . ?E)
+       ("@home" . ?H)
+       ("@work" . ?W)
+       ("planning" . ?p))) 
+
+;; Configure custom agenda views
+(setq org-agenda-custom-commands
+      '(("d" "Dashboard"
+	 ((agenda "" ((org-deadline-warning-days 7)))
+	  (todo "NEXT"
+		((org-agenda-overriding-header "Next Tasks")))
+	  (tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+
+	("n" "Next Tasks"
+	 ((todo "NEXT"
+		((org-agenda-overriding-header "Next Tasks")))))
+
+	("W" "Work Tasks" tags-todo "+work-email")
+
+	;; Low-effort next actions
+	("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+	 ((org-agenda-overriding-header "Low Effort Tasks")
+	  (org-agenda-max-todos 20)
+	  (org-agenda-files org-agenda-files)))
+
+	("w" "Workflow Status"
+	 ((todo "WAIT"
+		((org-agenda-overriding-header "Waiting on External")
+		 (org-agenda-files org-agenda-files)))
+	  (todo "REVIEW"
+		((org-agenda-overriding-header "In Review")
+		 (org-agenda-files org-agenda-files)))
+	  (todo "PLAN"
+		((org-agenda-overriding-header "In Planning")
+		 (org-agenda-todo-list-sublevels nil)
+		 (org-agenda-files org-agenda-files)))
+	  (todo "BACKLOG"
+		((org-agenda-overriding-header "Project Backlog")
+		 (org-agenda-todo-list-sublevels nil)
+		 (org-agenda-files org-agenda-files)))
+	  (todo "READY"
+		((org-agenda-overriding-header "Ready for Work")
+		 (org-agenda-files org-agenda-files)))
+	  (todo "ACTIVE"
+		((org-agenda-overriding-header "Active Projects")
+		 (org-agenda-files org-agenda-files)))
+	  (todo "COMPLETED"
+		((org-agenda-overriding-header "Completed Projects")
+		 (org-agenda-files org-agenda-files)))
+	  (todo "CANC"
+		((org-agenda-overriding-header "Cancelled Projects")
+		 (org-agenda-files org-agenda-files)))))))
+
+(setq org-capture-templates
+    `(("t" "Tasks / Projects")
+      ("tt" "Task" entry (file+olp "~/dump/tasks.org" "Inbox")
+           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+
+      ("j" "Journal Entries")
+      ("jj" "Journal" entry
+           (file+olp+datetree "~/dump/journal.org")
+           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+           ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
+           :clock-in :clock-resume
+           :empty-lines 1)
+      ("jm" "Meeting" entry
+           (file+olp+datetree "~/dump/journal.org")
+           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+           :clock-in :clock-resume
+           :empty-lines 1)
+
+      ("w" "Workflows")
+      ("we" "Checking Email" entry (file+olp+datetree "~/dump/journal.org")
+           "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
+
+      ("m" "Metrics Capture")
+      ("mw" "Weight" table-line (file+headline "~/dump/metrics.org" "Weight")
+       "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
+
+;; Journal 
+(define-key global-map (kbd "C-c j")
+  (lambda () (interactive) (org-capture nil "jj"))) 
+
+
 
 (defun efs/org-mode-visual-fill ()
   (setq visual-fill-column-width 125
